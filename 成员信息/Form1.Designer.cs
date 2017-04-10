6b2ad6d894a7f3da50c6041b7d9975282badf97d
家辉 邓 <919344636@qq.com>
2017-04-10 17:41:11 +0800

    添加项目文件。

---
+++ 成员信息/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace 成员信息
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.姓名 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.学号 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.电话 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.memberBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.memberDataSet2 = new 成员信息.memberDataSet2();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.memberTableAdapter1 = new 成员信息.memberDataSet2TableAdapters.memberTableAdapter();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBoxSerialPortName = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.timerscanserial = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.刷新数据库ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存更改ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.查找ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memberBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memberDataSet2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.姓名,
+            this.学号,
+            this.电话});
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.DataSource = this.memberBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(682, 464);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // 姓名
+            // 
+            this.姓名.DataPropertyName = "姓名";
+            this.姓名.HeaderText = "姓名";
+            this.姓名.Name = "姓名";
+            // 
+            // 学号
+            // 
+            this.学号.DataPropertyName = "学号";
+            this.学号.HeaderText = "学号";
+            this.学号.Name = "学号";
+            // 
+            // 电话
+            // 
+            this.电话.DataPropertyName = "电话";
+            this.电话.HeaderText = "电话";
+            this.电话.Name = "电话";
+            // 
+            // memberBindingSource1
+            // 
+            this.memberBindingSource1.DataMember = "member";
+            this.memberBindingSource1.DataSource = this.memberDataSet2;
+            // 
+            // memberDataSet2
+            // 
+            this.memberDataSet2.DataSetName = "memberDataSet2";
+            this.memberDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ContextMenuStrip = this.contextMenuStrip2;
+            this.textBox1.Location = new System.Drawing.Point(16, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 25);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // memberTableAdapter1
+            // 
+            this.memberTableAdapter1.ClearBeforeFill = true;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(35, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 34);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "打开串口";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBoxSerialPortName
+            // 
+            this.comboBoxSerialPortName.FormattingEnabled = true;
+            this.comboBoxSerialPortName.Location = new System.Drawing.Point(16, 24);
+            this.comboBoxSerialPortName.Name = "comboBoxSerialPortName";
+            this.comboBoxSerialPortName.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxSerialPortName.TabIndex = 8;
+            // 
+            // comboBoxBaudRate
+            // 
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(16, 53);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxBaudRate.TabIndex = 9;
+            // 
+            // timerscanserial
+            // 
+            this.timerscanserial.Enabled = true;
+            this.timerscanserial.Interval = 1000;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.comboBoxBaudRate);
+            this.groupBox1.Controls.Add(this.comboBoxSerialPortName);
+            this.groupBox1.Location = new System.Drawing.Point(700, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(152, 175);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "卡号";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(700, 207);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(152, 163);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "信息";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "电话";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "学号";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "姓名";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(49, 132);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(94, 25);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(49, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(94, 25);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(94, 25);
+            this.textBox2.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.刷新数据库ToolStripMenuItem,
+            this.保存更改ToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(160, 56);
+            // 
+            // 刷新数据库ToolStripMenuItem
+            // 
+            this.刷新数据库ToolStripMenuItem.Name = "刷新数据库ToolStripMenuItem";
+            this.刷新数据库ToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.刷新数据库ToolStripMenuItem.Text = "刷新数据库";
+            this.刷新数据库ToolStripMenuItem.Click += new System.EventHandler(this.刷新数据库ToolStripMenuItem_Click);
+            // 
+            // 保存更改ToolStripMenuItem1
+            // 
+            this.保存更改ToolStripMenuItem1.Name = "保存更改ToolStripMenuItem1";
+            this.保存更改ToolStripMenuItem1.Size = new System.Drawing.Size(159, 26);
+            this.保存更改ToolStripMenuItem1.Text = "保存更改";
+            this.保存更改ToolStripMenuItem1.Click += new System.EventHandler(this.保存更改ToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.查找ToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(115, 30);
+            // 
+            // 查找ToolStripMenuItem
+            // 
+            this.查找ToolStripMenuItem.Name = "查找ToolStripMenuItem";
+            this.查找ToolStripMenuItem.Size = new System.Drawing.Size(114, 26);
+            this.查找ToolStripMenuItem.Text = "查找";
+            this.查找ToolStripMenuItem.Click += new System.EventHandler(this.查找ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 488);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memberBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memberDataSet2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private memberDataSet2 memberDataSet2;
+        private System.Windows.Forms.BindingSource memberBindingSource1;
+        private memberDataSet2TableAdapters.memberTableAdapter memberTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 姓名;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 学号;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 电话;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBoxSerialPortName;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.Timer timerscanserial;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 刷新数据库ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存更改ToolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem 查找ToolStripMenuItem;
+    }
+}
+
---
+++ 成员信息/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 成员信息
+{
+    public partial class Form1 : Form
+    {
+        string strConnection;
+        SqlDataAdapter adapter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“memberDataSet2.member”中。您可以根据需要移动或删除它。
+            this.memberTableAdapter1.Fill(this.memberDataSet2.member);
+            memberDataSet2.EnforceConstraints = false;
+            try
+            {
+                strConnection = "Server=(localdb)\\ProjectsV13;";//选择服务器
+                strConnection += "Initial Catalog=member;";      //选择表
+                strConnection += "User Id=ddd;";                 //用户名
+                strConnection += "Password=ddd;";                //密码
+                strConnection += "Connect Timeout=5";            //超时
+                adapter = new SqlDataAdapter("select * from member", strConnection);
+                SqlConnection conn = new SqlConnection(strConnection);
+                SqlCommand SCD = new SqlCommand("select * from member ", conn);
+                adapter.SelectCommand = SCD;
+                
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder myCommandBuilder = new SqlCommandBuilder(adapter);
+            try
+            {
+                adapter.Update(memberDataSet2, "member");
+                this.memberTableAdapter1.Fill(this.memberDataSet2.member);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.memberTableAdapter1.Fill(this.memberDataSet2.member);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = ("select * from member where ID=" + textBox1.Text);
+                SqlConnection conn = new SqlConnection(strConnection);
+                SqlCommand SCD = new SqlCommand(str, conn);
+                conn.Open();
+                SqlDataReader reder = SCD.ExecuteReader();
+                if (reder.Read())
+                    label1.Text = reder["电话"].ToString();
+                else
+                    MessageBox.Show("no this message");
+                conn.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string portRead = serialPort1.ReadLine();
+            Action showReceive = () =>
+            {
+                textBox1.Text= portRead;
+            };
+            this.Invoke(showReceive);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Close();
+                    button4.Text = "打开串口";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    button4.Text = "关闭串口";
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.PortName = comboBoxSerialPortName.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBoxBaudRate.Text);
+                    serialPort1.Open();
+                    button4.Text = "关闭串口";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    button4.Text = "打开串口";
+                }
+            }
+        }
+        private void timerscanserial_Tick(object sender, EventArgs e)
+        {
+            scanSerial();
+        }
+        int portNamesLength = 0;
+        void scanSerial()
+        {
+            try
+            {
+                string[] portNames = SerialPort.GetPortNames();
+                if (portNames.Length != portNamesLength)
+                {
+                    comboBoxSerialPortName.Items.Clear();
+                    comboBoxSerialPortName.Items.AddRange(portNames);
+                    comboBoxSerialPortName.SelectedIndex = 0;
+                    portNamesLength = portNames.Length;
+                }
+            }
+            catch//(Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void 刷新数据库ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.memberTableAdapter1.Fill(this.memberDataSet2.member);
+        }
+
+        private void 保存更改ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder myCommandBuilder = new SqlCommandBuilder(adapter);
+            try
+            {
+                adapter.Update(memberDataSet2, "member");
+                this.memberTableAdapter1.Fill(this.memberDataSet2.member);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void 查找ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = ("select * from member where ID=" + textBox1.Text);
+                SqlConnection conn = new SqlConnection(strConnection);
+                SqlCommand SCD = new SqlCommand(str, conn);
+                conn.Open();
+                SqlDataReader reder = SCD.ExecuteReader();
+                if (reder.Read())
+                {
+                    textBox2.Text = reder["姓名"].ToString();
+                    textBox3.Text = reder["学号"].ToString();
+                    textBox4.Text = reder["电话"].ToString();
+                }
+                else
+                    MessageBox.Show("no this message");
+                conn.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
